2bf174ae6aeab34d80167cd379fa3a87c4c86d5d
natsuokawai <kawai.natsuo@gmail.com>
2020-08-24 20:20:22 +0900

    add controllers

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::BooksController < ApplicationController
+  def create; end
+
+  def show; end
+
+  def update; end
+
+  def destroy; end
+end
---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::UsersController < ApplicationController
+  def index; end
+  def update; end
+end
